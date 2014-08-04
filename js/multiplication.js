e0937c6625577499a8b32b0ec10dd8a731ca3aa9
acidaris <agreenfield@payjunction.com>
2014-08-04 13:08:16 -0700

    styling and highlighting.

--- js/multiplication.js
+++ js/multiplication.js
@@ -11,8 +11,25 @@ angular.module('myApp', [])
 
 		$scope.numberLimit = $attrs.initialNumberLimit || 10;
 		$scope.numbers = populateNumbers($scope.numberLimit);
-		$scope.multiply = function(a,b){
-			return a*b;
+		$scope.multiply = function ( a, b ) {
+			return a * b;
+		};
+		$scope.$watch('numberLimit', function ( limit ) {
+			$scope.numbers = populateNumbers(limit);
+		});
+
+		var activeFactorA, activeFactorB;
+		$scope.setActiveFactors = function ( a, b ) {
+			activeFactorA = a;
+			activeFactorB = b;
+		};
+
+		$scope.matchesFactor = function ( a, b ) {
+			return a === activeFactorA || b === activeFactorB;
+		};
+
+		$scope.clearActiveFactors = function () {
+			activeFactorA = activeFactorB = null;
 		};
 
 	});
