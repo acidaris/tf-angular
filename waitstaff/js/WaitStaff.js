1ca32b95f5d3396c8554d2c167e32bb891c2f500
acidaris <agreenfield@payjunction.com>
2014-08-12 15:19:00 -0700

    waitstaff project for lesson 3

---
+++ waitstaff/js/WaitStaff.js
@@ -0,0 +1,104 @@
+angular.module("waitStaff", [])
+		.controller("mealDetails", function ( $scope, $rootScope ) {
+
+			$scope.init = function () {
+				$scope.clearFields();
+			};
+
+			$scope.recordDetails = function () {
+				if ($scope.details.$valid) {
+					var data = {
+						base : $scope.mealBase,
+						taxPercent : $scope.taxRate,
+						tipPercent : $scope.tipPercent
+					};
+					$rootScope.$broadcast('mealSubmitted', data);
+				} else {
+					console.log("invalid");
+				}
+			};
+
+			$scope.clearFields = function () {
+				$scope.mealBase = '';
+				$scope.taxRate = 0;
+				$scope.tipPercent = 0;
+			};
+
+			$scope.$on('reset', function ( event, data ) {
+				$scope.clearFields();
+			});
+
+			$scope.init();
+
+		}).controller("customerCharges", function ( $scope ) {
+
+			$scope.init = function () {
+				$scope.clearFields();
+			};
+
+			$scope.clearFields = function () {
+				$scope.subtotal = 0;
+				$scope.tip = 0;
+				$scope.total = 0;
+			};
+
+
+			$scope.$on('mealSubmitted', function ( event, data ) {
+
+				var subTotal = calculateSubTotal(data.base, data.taxPercent);;
+				var tip = subTotal * data.tipPercent / 100;
+				var total = subTotal + tip;
+
+				$scope.subtotal += subTotal;
+				$scope.tip += tip;
+				$scope.total += total;
+			});
+
+			$scope.$on('reset', function ( event, data ) {
+				$scope.clearFields();
+			});
+
+			$scope.init();
+
+
+		}).controller("myEarningInfo", function ( $scope ) {
+
+			$scope.init=function()
+			{
+				$scope.clearFields();
+			};
+
+			$scope.clearFields=function()
+			{
+				$scope.tipTotal = 0;
+				$scope.mealCount = 0;
+				$scope.averageTip = 0;
+			};
+
+			$scope.$on('mealSubmitted', function ( event, data ) {
+
+				var subTotal = calculateSubTotal(data.base,data.taxPercent);
+
+				$scope.tipTotal += subTotal * data.tipPercent / 100;
+				$scope.mealCount++;
+
+			});
+
+			$scope.$on('reset', function ( event, data ) {
+				$scope.clearFields();
+			});
+
+			$scope.init();
+		}).controller("resetCtrl", function ( $scope, $rootScope ) {
+			$scope.reset = function () {
+				$rootScope.$broadcast('reset', {});
+			};
+
+		})
+;
+
+function calculateSubTotal(base,taxPercent)
+{
+	return base + (base * taxPercent / 100);
+}
+
