b10d64d408545fe7e7a2346d54ee8e30a4583f54
acidaris <agreenfield@payjunction.com>
2014-08-20 15:26:25 -0700

    madlibs with animation and stagger.

--- madlibs-animated/js/MadLibs.js
+++ madlibs-animated/js/MadLibs.js
@@ -9,15 +9,15 @@ angular.module("myApp", ['ngAnimate']).controller("madlibs", function ( $scope )
 
 
 	$scope.clearFields = function () {
-		$scope.femaleName = 'a';
-		$scope.jobTitle = 'b';
-		$scope.tediousTask = 'c';
-		$scope.dirtyTask = 'd';
-		$scope.celebrity = 'e';
-		$scope.uselessSkill = 'f';
-		$scope.obnoxiousCelebrity = 'g';
-		$scope.hugeNumber = '2';
-		$scope.adjective = 'e';
+		$scope.name = 'Adam';
+		$scope.jobTitle = 'lifeguard';
+		$scope.tediousTask = 'teaching swim lessons';
+		$scope.dirtyTask = 'cleaning gutters';
+		$scope.celebrity = 'Michael Phelps';
+		$scope.uselessSkill = 'wiggle his ears';
+		$scope.obnoxiousCelebrity = 'Justin Beiber';
+		$scope.hugeNumber = '15000';
+		$scope.adjective = 'crusty';
 		$scope.gender='male';
 	};
 
