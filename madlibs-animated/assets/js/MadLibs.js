6a63789ead2f5f03f7d2fa5a7cb16f2f31cfa8b3
acidaris <agreenfield@payjunction.com>
2014-08-20 15:51:10 -0700

    changing animation method.

---
+++ madlibs-animated/assets/js/MadLibs.js
@@ -0,0 +1,60 @@
+angular.module("myApp", ['ngAnimate']).controller("madlibs", function ( $scope ) {
+
+	$scope.invalidHugeNumber=false;
+
+	$scope.startOver = function () {
+		$scope.generated = false;
+		$scope.clearFields();
+	};
+
+    $scope.loadFields = function()
+    {
+        $scope.name = 'Adam';
+        $scope.jobTitle = 'lifeguard';
+        $scope.tediousTask = 'teaching swim lessons';
+        $scope.dirtyTask = 'cleaning gutters';
+        $scope.celebrity = 'Michael Phelps';
+        $scope.uselessSkill = 'wiggle his ears';
+        $scope.obnoxiousCelebrity = 'Justin Beiber';
+        $scope.hugeNumber = '15000';
+        $scope.adjective = 'crusty';
+        $scope.gender = 'male';   
+    };
+    
+	$scope.clearFields = function () {
+		$scope.name = '';
+		$scope.jobTitle = '';
+		$scope.tediousTask = '';
+		$scope.dirtyTask = '';
+		$scope.celebrity = '';
+		$scope.uselessSkill = '';
+		$scope.obnoxiousCelebrity = '';
+		$scope.hugeNumber = '';
+		$scope.adjective = '';
+		$scope.gender='';
+	};
+
+	$scope.clearFields();
+
+
+	$scope.generateMadLib = function () {
+
+		$scope.invalidHugeNumber = false;
+
+		if ($scope.madlibFields.$valid) {
+			$scope.generated = true;
+		} else {
+			$scope.setPatternErrors($scope.madlibFields.hugeNumber.$error);
+		}
+	};
+
+	$scope.setPatternErrors = function ( errors ) {
+		var patternErrors = errors.pattern;
+		if (patternErrors) {
+			$scope.invalidHugeNumber=true;
+		}
+	};
+
+	$scope.generated = false;
+});
+
