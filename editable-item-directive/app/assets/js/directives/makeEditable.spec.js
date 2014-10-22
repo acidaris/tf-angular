ef024b13d73ab31997eb34e040d8b8887498904f
acidaris <agreenfield@payjunction.com>
2014-10-22 16:18:50 -0700

    basic test for editable item directive.

--- editable-item-directive/app/assets/js/directives/makeEditable.spec.js
+++ editable-item-directive/app/assets/js/directives/makeEditable.spec.js
@@ -1,9 +1,20 @@
 describe('makeEditable directive tests',function(){
 
-  beforeEach(module('countriesAndCapitals'));
+  var $scope, element;
+
+  beforeEach(module('EditableItem.makeEditable'));
   beforeEach(module('./assets/partials/make-editable.html'));
 
-  it('', function() {
+  beforeEach(inject(function(_$rootScope_, $compile) {
+    $scope = _$rootScope_.$new();
 
+    element = $compile('<div make-editable><p>Edit this</p></div>')($scope);
+    $scope.$digest();
+
+  }));
+
+  it('content transcluded into correct location', function() {
+    debugger;
+      expect(element.find('div.content p').text()).toEqual('Edit this');
   });
 });
