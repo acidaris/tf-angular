d862fe2e104f6a1672a8c18082934496fb41bcaf
acidaris <agreenfield@payjunction.com>
2014-10-22 12:35:31 -0700

    editable item directive functionality.

--- editable-item-directive/app/assets/js/directives/makeEditable.js
+++ editable-item-directive/app/assets/js/directives/makeEditable.js
@@ -1,7 +1,23 @@
-angular.module('EditableItem.makeEditable',[]).directive('makeEditable',function(){
+angular.module('EditableItem.makeEditable', []).directive('makeEditable', function() {
   return {
-    restrict:'A',
-    transclude:true,
-    templateUrl:"./assets/partials/make-editable.html"
+    restrict : 'A',
+    transclude : true,
+    scope:true,
+    templateUrl : "./assets/partials/make-editable.html",
+    controller : function($scope, $element, $timeout) {
+
+      var editableItem = $element.find('div')[2];
+
+      $scope.editable = false;
+      $scope.edit = function() {
+        $scope.editable = true;
+        $timeout(function() {
+          editableItem.focus();
+        }, 300);
+      };
+      $scope.save = function() {
+        $scope.editable = false;
+      };
+    }
   };
-})
+});
